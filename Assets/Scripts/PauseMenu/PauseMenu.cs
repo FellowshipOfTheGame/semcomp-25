b8ed408819ceeff7bea91b6d091eedc6a74cbf85
Tyago Yuji Teoi <tyago.teoi@usp.br>
2022-06-20 02:44:38 -0300

    Add Pause Menu

---
+++ Assets/Scripts/PauseMenu/PauseMenu.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isGamePaused = false;
+
+    // Receive the PauseMenu game object
+    public GameObject pauseMenuUI;
+    public GameObject BackgroundImage;
+
+        // Update is called once per frame
+    void Update()
+    {
+        // Check if the user press ESC button
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // IF the game is already paused, exits the pause menu
+            if (isGamePaused)
+            {
+                Resume();
+            } 
+            // ELSE join the pause menu
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    // Exit the Pause Menu
+    public void Resume()
+    {
+        // Exit the Pause Menu in the canvas
+        pauseMenuUI.SetActive(false);
+        BackgroundImage.SetActive(false);
+
+        // Unfreeze the game
+        Time.timeScale = 1f;
+
+        // Set false meaning that the game is NOT paused
+        isGamePaused = false;
+    }
+
+    // Open the Pause Menu
+    void Pause()
+    {
+        // Show the Pause Menu in the canvas
+        pauseMenuUI.SetActive(true);
+
+        // Freeze the game
+        Time.timeScale = 0f;
+
+        // Set true meaning that the game is paused
+        isGamePaused = true;
+
+        BackgroundImage.SetActive(true);
+    }
+
+    // Open the Main Menu
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    // Quit the game
+    public void QuitGame()
+    {
+        Debug.Log("Quit the game");
+        Application.Quit();
+    }
+}
