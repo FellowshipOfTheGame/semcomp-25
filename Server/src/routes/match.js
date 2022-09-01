8266174084b5463e1f2fc0851edb84bcd6b9a3c0
Marcus Pará <marcuspara66@usp.br>
2022-09-01 08:48:11 -0300

    [server-match-start] creating endpoint for match

---
+++ Server/src/routes/match.js
@@ -0,0 +1,14 @@
+// Dependencies
+const express = require('express');
+const routes = express.Router();
+
+const configEnv = require('../config')
+
+// Middlewares
+const SessionMiddleware = require('../middlewares/Session.middleware');
+
+routes.get('/', (req, res) => {
+    res.status(200).send("Match alive!");
+})
+
+module.exports = routes;
