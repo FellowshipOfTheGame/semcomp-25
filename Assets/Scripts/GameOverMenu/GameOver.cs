ae58fb10f636ec246a10fd16b6e1dfdc443ca8bc
tyago-teoi <tyago.teoi@usp.br>
2022-09-14 20:00:53 -0300

    Fix marmosset spawn to gameover menu

--- Assets/Scripts/GameOverMenu/GameOver.cs
+++ Assets/Scripts/GameOverMenu/GameOver.cs
@@ -9,10 +9,18 @@ public class GameOver : MonoBehaviour
     [SerializeField] private TMP_Text _gameOverPointsView;
     [SerializeField] private TMP_Text _highScoreView;
     [SerializeField] private GameObject _menu;
-    [SerializeField] private GameObject _marmosset;
+    [SerializeField] private GameObject _sadMarmossets;
+    [SerializeField] private GameObject _neutralMarmossets;
+    [SerializeField] private GameObject _happyMarmossets;
+
+    private GameObject _currentMarmoset;
 
     private ScoreSystem _scoreSystem;
     private int highscore = 0;
+
+    private const short HAPPY = 0;
+    private const short NEUTRAL = 1;
+    private const short SAD = 2;
 
     void Awake()
     {
@@ -27,17 +35,31 @@ public class GameOver : MonoBehaviour
     private void Start()
     {
         highscore = PlayerPrefs.GetInt("HighScore", 0); // Antes disso, checar se é a primeira vez que o jogador está jogando?
+        _currentMarmoset = _happyMarmossets.transform.GetChild(0).gameObject;
     }
 
-    private void LoadMarmosset()
+    private void LoadMarmosset(int scoreState)
     {
-        // Deactivate all marmosset in the gameover menu
-        foreach (Transform marmosset in _marmosset.transform)
-            marmosset.gameObject.SetActive(false);
+        int randomMarmossetIndex;
 
-        // Then, activate a random marmosset
-        int randomMarmossetIndex = Random.Range(0, _marmosset.transform.childCount);
-        _marmosset.transform.GetChild(randomMarmossetIndex).gameObject.SetActive(true);
+        _currentMarmoset.SetActive(false);
+
+        switch(scoreState)
+        {
+            case HAPPY:
+                randomMarmossetIndex = Random.Range(0, _happyMarmossets.transform.childCount);
+                _currentMarmoset = _happyMarmossets.transform.GetChild(randomMarmossetIndex).gameObject;
+                break;
+            case NEUTRAL:
+                randomMarmossetIndex = Random.Range(0, _neutralMarmossets.transform.childCount);
+                _currentMarmoset = _neutralMarmossets.transform.GetChild(randomMarmossetIndex).gameObject;
+                break;
+            case SAD:
+                randomMarmossetIndex = Random.Range(0, _sadMarmossets.transform.childCount);
+                _currentMarmoset = _sadMarmossets.transform.GetChild(randomMarmossetIndex).gameObject;
+                break;
+        }
+        _currentMarmoset.SetActive(true);
     }
 
     public void OnGameOver()
@@ -46,9 +68,14 @@ public class GameOver : MonoBehaviour
 
         //_scoreSystem.PrintScore();
 
-        LoadMarmosset();
-
         GameOverPointbar.Instance.LoadPointBar();
+
+        if (totalPoints < highscore * 0.3)
+            LoadMarmosset(SAD);
+        else if (totalPoints > highscore)
+            LoadMarmosset(HAPPY);
+        else
+            LoadMarmosset(NEUTRAL);
 
         highscore = (int)Mathf.Max(highscore, totalPoints);
 
