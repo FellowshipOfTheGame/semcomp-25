635edffd4d0493312310a184679d06be6c3ab9ce
lucasebling <lucasebling@bol.com.br>
2022-09-22 00:11:59 -0300

    Changes to PowerupSpawn System
    Made some chances to how power ups are randomly selected so that we may have different percentages easier.

--- Assets/SpawnRandom.cs
+++ Assets/SpawnRandom.cs
@@ -5,15 +5,49 @@ using UnityEngine;
 public class SpawnRandom : MonoBehaviour
 {
     [SerializeField] List<GameObject> prefabs;
+
+    //SpawnChance => Define qual é a chance de um dado prefab aparecer. 
+    [SerializeField] List<int> spawnChance;
     GameObject objSpawned;
     public void Spawn()
     {
-        objSpawned=Instantiate(prefabs[Random.Range(0, prefabs.Count)], transform);
+        int index =  setIndex();
+        objSpawned=Instantiate(prefabs[index], transform);
+        //objSpawned=Instantiate(prefabs[Random.Range(0, prefabs.Count)], transform);
+    
     }
 
     void Awake()
     {
         Spawn();
+    }
+
+    int setIndex(){
+
+        int totalSum = 0;
+
+        //Soma total, que é usada para tirar um número aleatório.
+        for(int i = 0; i < spawnChance.Count; i++){
+            totalSum += spawnChance[i];
+        }
+
+        int thresHold = Random.Range(0,totalSum);
+
+
+        int sum = 0;
+
+        int index = 0;
+        //se o Threshold for menor ou igual a soma atual, selecionamos esse valor e damos break
+        for(int i = 0; i < spawnChance.Count; i++){
+            sum += spawnChance[i];
+
+            if(sum >= thresHold){
+                index = i;
+                return index;
+            }
+        }
+
+        return index;
     }
 
 }
